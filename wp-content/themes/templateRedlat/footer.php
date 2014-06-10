58acce4221f4f86f7c4f58b14fc7de8b4038718c
juan2ramos <juan2ramos@gmail.com>
2014-06-10 16:53:06 -0500

    + Analitycs

--- wp-content/themes/templateRedlat/footer.php
+++ wp-content/themes/templateRedlat/footer.php
@@ -23,11 +23,19 @@
         	<!-- Google Analytics - Optimized version by Mathias Bynens -->
 	<!-- See: http://mathiasbynens.be/notes/async-analytics-snippet -->
 	<!-- Change the UA-XXXX-XX string to your site's ID -->
-	<script>
-		var _gaq=[['_setAccount','UA-XXXX-XX'],['_trackPageview']];(function(a,b){var c=a.createElement(b),d=a.getElementsByTagName(b)[0];c.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";d.parentNode.insertBefore(c,d)})(document,"script");
-	</script>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/jquery.flexslider-min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/script.js"></script>
-    </body>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-43178789-4', 'redalt.org');
+            ga('send', 'pageview');
+
+        </script>
+        </body>
 </html>
