aefa7e9726d96aa608116bd15c5f79f1d9cfb7b0
juan2ramos <juan2ramos@gmail.com>
2014-06-16 06:20:36 -0500

    + TimeLine

--- wp-content/themes/templateRedlat/historia.php
+++ wp-content/themes/templateRedlat/historia.php
@@ -30,7 +30,10 @@ Template Name: historia
     <section>
 
         <div class="dipity_embed" style="width:960px">
-            <iframe width="960" height="400" src="http://www.dipity.com/jenny_giraldo/Trayectoria-REDLAT/?mode=embed&z=0#tl" style="border:none;"></iframe>
+            <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="600" height="355" id="timerimeSWF" align="middle">
+                <param name="allowScriptAccess" value="always" />
+                <param name="movie" value="http://timerime.com/flash/timerimeSWF.swf?Qxml=3162592&embedded=1" /><param name="quality" value="high" /><param name="wmode" value="transparent" /><param name="bgcolor" value="#ffffff" /><embed src="http://timerime.com/flash/timerimeSWF.swf?Qxml=3162592&embedded=1" quality="high" wmode="transparent" bgcolor="#ffffff" width="600" height="355" name="timerimeSWF" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+            </object>
         </div>
     </section>
 </main>
